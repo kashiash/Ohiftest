44b9e16621c28308625f203366a8ce1869109eae
jacek <jacek@PIECYK11>
2023-02-15 19:44:55 +0100

    .

---
+++ ApiForTestOHIF/Controllers/MetadataController.cs
@@ -0,0 +1,62 @@
+﻿using ApiForTestOHIF.Models;
+using ApiForTestOHIF.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiForTestOHIF.Controllers
+{
+    public class MetadataController : ControllerBase
+    {
+        protected IWadoRsService WadoService { get; set; }
+
+        public MetadataController
+        (
+            IWadoRsService wadoService
+        )
+        {
+            WadoService = wadoService;
+        }
+
+        [HttpGet]
+        [Route("wadors/studies/{StudyInstanceUID}/metadata")]
+        [Route("api/studies/{StudyInstanceUID}/metadata")]
+        public HttpResponseMessage GetStudiesMetadata
+        (
+             [FromQuery]
+            IWadoRsStudiesRequest request
+        )
+        {
+            return WadoService.RetrieveStudyMetadata(request);
+        }
+
+        [HttpGet]
+        [Route("wadors/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/metadata")]
+        [Route("api/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/metadata")]
+        public HttpResponseMessage GetSeriesMetadata
+        (
+            [FromQuery]
+            IWadoRsSeriesRequest request
+        )
+        {
+            return WadoService.RetrieveSeriesMetadata(request);
+        }
+
+        [HttpGet]
+        [Route("wadors/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances/{SOPInstanceUID}/metadata")]
+        [Route("api/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances/{SOPInstanceUID}/metadata")]
+        public HttpResponseMessage GetInstanceMetadata
+        (
+            [FromQuery]
+            IWadoRsInstanceRequest request
+        )
+        {
+            try
+            {
+                return WadoService.RetrieveInstanceMetadata(request);
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage() { Content = new StringContent(ex.ToJson()) };
+            }
+        }
+    }
+}
--- ApiForTestOHIF/Controllers/WadoUriController.cs
+++ ApiForTestOHIF/Controllers/WadoUriController.cs
@@ -1,6 +1,29 @@
-﻿namespace ApiForTestOHIF.Controllers
+﻿using ApiForTestOHIF.Models;
+using ApiForTestOHIF.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ApiForTestOHIF.Controllers
 {
-    public class Class
+    public class WadoUriController : ControllerBase
     {
+        IWadoUriService ServiceHandler { get; set; }
+
+        public WadoUriController(IWadoUriService serviceHandler)
+        {
+            ServiceHandler = serviceHandler;
+        }
+
+        [Route("wadouri")]
+        [Route("api/wadouri")]
+        public HttpResponseMessage Get
+        (
+            [FromQuery] WadoUriRequest request
+        )
+        {
+            if (null == request) { return new HttpResponseMessage(HttpStatusCode.BadRequest); }
+
+            return ServiceHandler.GetInstance(request);
+        }
     }
 }
